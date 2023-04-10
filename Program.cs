74b90d0883500080a1a9c943cbe144c740639191
Kimosable <129333154+Kimosable@users.noreply.github.com>
2023-04-10 23:14:06 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,14 +2,9 @@
 int i = int.Parse(Console.ReadLine());
 Console.Write("Введите число b: ");
 int j = int.Parse(Console.ReadLine());
-Console.Write("Введите число c: ");
-int k = int.Parse(Console.ReadLine());
-
-int max = i;
-
-if(i > max) max = i;
-if(j > max) max = j;
-if(k > max) max = k;
-
-Console.Write("max = ");
-Console.WriteLine(max);
+if (i > j) {
+    Console.WriteLine(i);
+}
+else{
+    Console.WriteLine(j);
+}
